bf954f5a5226e5cf47ef8f1db11cb6a5d4aa9d47
st0ne <rob@st0ne.at>
2013-04-04 23:53:31 +0200

    keep url parameters while reorder

--- orderedmodel/admin.py
+++ orderedmodel/admin.py
@@ -19,13 +19,17 @@ class OrderedModelAdmin(admin.ModelAdmin):
     return my_urls + super(OrderedModelAdmin, self).get_urls()
 
   def reorder(self, item):
-    button = '<a href="{{0}}/move_{{1}}/"><img src="{0}orderedmodel/arrow-{{1}}.gif" alt="{{1}}" /></a>'.format(settings.STATIC_URL)
+    button = '<a href="{{0}}/move_{{1}}/?{{2}}"><img src="{0}orderedmodel/arrow-{{1}}.gif" alt="{{1}}" /></a>'.format(settings.STATIC_URL)
 
     html = ''
-    html += button.format(item.pk, 'down')
-    html += '&nbsp;' + button.format(item.pk, 'up')
+    html += button.format(item.pk, 'down', self.params )
+    html += '&nbsp;' + button.format(item.pk, 'up', self.params )
     return html
   reorder.allow_tags = True
+
+  def changelist_view(self, request, extra_context=None):
+    self.params = request.GET.urlencode()
+    return super(OrderedModelAdmin,self).changelist_view(request, extra_context=extra_context)
 
   def move_down(self, request, pk):
     if self.has_change_permission(request):
@@ -36,7 +40,7 @@ class OrderedModelAdmin(admin.ModelAdmin):
         pass
       else:
         self.model.swap(item, next_item)
-    return HttpResponseRedirect('../../')
+    return HttpResponseRedirect('../../?' + request.GET.urlencode())
 
   def move_up(self, request, pk):
     if self.has_change_permission(request):
@@ -47,5 +51,6 @@ class OrderedModelAdmin(admin.ModelAdmin):
         pass
       else:
         self.model.swap(item, prev_item)
-    return HttpResponseRedirect('../../')
+    #print dir(request.GET)
+    return HttpResponseRedirect('../../?' + request.GET.urlencode())
 

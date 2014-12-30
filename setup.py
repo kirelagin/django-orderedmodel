cbc21e0b92cb3b7e8974d89490877832fa6a103a
Kirill Elagin <kirelagin@gmail.com>
2014-12-30 12:42:20 +0300

    Awesome `setup.py`
    Closes #2.
    Fixes #7.
    Closes #8.

--- setup.py
+++ setup.py
@@ -1,20 +1,45 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
-setup(
-    name='django-orderedmodel',
-    version='1.0',
-    description='intends to help you create Django models which can be moved up\down (or left\right) with respect to each other.',
-    author='Kirill Elagin',
-    author_email='kirelagin@gmail.com',
-    url='https://github.com/kirelagin/django-orderedmodel',
-    packages = [
-        'orderedmodel'
-    ],
-    package_data = {'orderedmodel': [
-        'static/orderedmodel/arrow-down.gif',
-        'static/orderedmodel/arrow-up.gif'
-    ]}
-)
+
+setup(name='django-orderedmodel',
+      version='2014.12.001',
+
+      description='Orderable models for Django',
+      long_description='''
+          This Django_ app helps you create Django models that can be
+          moved up/down with respect to each other.
+
+          See README_ for more details.
+
+          .. _Django: https://www.djangoproject.com/
+          .. _README: https://github.com/kirelagin/django-orderedmodel/blob/master/README.md
+      ''',
 
 
+      author='Kirill Elagin',
+      author_email='kirelagin@gmail.com',
 
+      url='https://github.com/kirelagin/django-orderedmodel',
+
+      classifiers = ['Development Status :: 5 - Production/Stable',
+                     'Environment :: Web Environment',
+                     'Framework :: Django',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: BSD License',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python :: 2',
+                     'Programming Language :: Python :: 3',
+                     'Topic :: Software Development :: Libraries',
+                    ],
+      keywords = ['Django', 'order', 'ordering', 'models'],
+
+      packages = ['orderedmodel'],
+      package_data = {
+        'orderedmodel': [
+            'static/orderedmodel/arrow-down.gif',
+            'static/orderedmodel/arrow-up.gif',
+        ],
+      },
+     )
